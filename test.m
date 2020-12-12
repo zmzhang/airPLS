27db3892491fa31b95a3a7318eb0a3bde5926134
zmzhang <zhangzhimin.csu@gmail.com>
2020-12-12 21:45:37 +0800

    Add test for single type

--- test.m
+++ test.m
@@ -2,9 +2,26 @@ clear
 close all
 
 load p1p2
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  double type test 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 x=p1;
 lambda=10e4;
+[xbc,xb]=airPLS(x, lambda,2,0.05);
 
+figure
+plot(x,'r')
+hold on
+plot(xbc,'g')
+plot(xb,'k')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  single type test 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+x=single(p1);
+lambda=10e4;
 [xbc,xb]=airPLS(x, lambda,2,0.05);
 
 figure
